b588ff8051040692406e2fbde1d37f988db0577f
mr.Rhenium <mr.rhenium75614@gmail.com>
2021-11-26 20:04:48 +0530

    snapchat button added

--- files/aboutuspage.js
+++ files/aboutuspage.js
@@ -1,20 +1,5 @@
 const phone = document.querySelector("#phone");
-phone.addEventListener("click",()=>{
-     var number=prompt("Enter the mobile no.: ");
-     phone.setAttribute("href",`tel:${number}`);
+phone.addEventListener("click", () => {
+  var number = prompt("Enter the mobile no.: ");
+  phone.setAttribute("href", `tel:${number}`);
 });
-
-
-
-//else if(feed_msg.value==""&&feed_nam==""){
-         
-     //      if(reaction[0].storage_like==""){
-     //           alert(`please!!! fill the like section`);
-     //      }else if(reaction[0].storage_star==""){
-     //          alert(`please!!! give the rating`);
-     //      }else{ 
-     //           localStorage.setItem("like_reaction",JSON.stringify(reaction));
-     //           alert(`Your Review is succesfully Post`);
-     //           console.log(reaction[0].storage_star);
-     //      };
-     // }
